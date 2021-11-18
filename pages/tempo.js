702bf00ef162ebd9fd138a7c496ff2710a4133fc
Marcos <marcos.junior@abion.com.br>
2021-11-18 14:46:21 -0300

    ajuste para local time

--- pages/tempo.js
+++ pages/tempo.js
@@ -3,16 +3,22 @@ import Link from 'next/link'
 
 function tempo(props){
     const dynamicDate = new Date()
-    const dynamicDateString = dynamicDate.toGMTString()
+    const dynamicDateString = dynamicDate.toUTCString()
     
+    const dynamicLocal = new Date()
+    const dynamicLocalString = dynamicLocal.toLocaleString()
+
     return (<div> 
         <div> <h1>Tempo</h1> </div>
         <Link href='/'>
             <a>Acessar página Home</a>
         </Link>
-        <div> {dynamicDateString} (dinâmico) </div>
-        <div> {props.staticDateString} (estático) </div>
         
+        <div> {dynamicDateString} (Einâmico) </div>
+        <div> {props.staticDateString} (Estático) </div>
+        <div> --------------------------------------------------------------------- </div>
+        <div> {dynamicLocalString} (Dinâmico Local) </div>
+        <div> {props.staticLocalString} (Estático Local) </div>
         
         </div>)
 
@@ -20,14 +26,21 @@ function tempo(props){
 
 export async function getStaticProps(){
     const staticDate = new Date()
-    const staticDateString = staticDate.toGMTString()
+    const staticDateString = staticDate.toUTCString()
+
+    const staticlocal = new Date()
+    const staticLocalString = staticlocal.toLocaleString()
 
     return {
         props: {
-            staticDateString
+            staticDateString,
+            staticLocalString
         },
         revalidate: 1
     }
 }
+
+// função que simula delay 
+// const delay = ms => new Promise(resolve => setTimeout(resolve,ms))
 
 export default tempo
